8afe845c9e0f1c94be26573af584bf312104dd46
Bond Davis <bonddavis92@gmail.com>
2017-07-31 15:44:20 -0500

    mostly done

--- bash.js
+++ bash.js
@@ -9,30 +9,35 @@ process.stdin.on('data', function (data) {
   var cmd = data.toString().trim().split(' '); // remove the newline
 
   if(cmd[0] === 'pwd'){
-    commands.pwd();
+    commands.pwd(done);
   }
   else if(cmd[0] === 'date'){
-    commands.date();
+    commands.date(done);
   }
   else if(cmd[0] === 'ls'){
-    commands.ls();
+    commands.ls(done);
   }
   else if (cmd[0] === 'echo') {
-    commands.echo(cmd);
+    commands.echo(cmd, done);
   }
   else if(cmd[0] === 'cat') {
-    commands.cat(cmd[1]);
+    commands.cat(cmd[1], done);
   }
   else if(cmd[0] === 'head') {
-    commands.head(cmd[1]);
+    commands.head(cmd[1], done);
   }
   else if(cmd[0] === 'tail') {
-    commands.tail(cmd[1]);
+    commands.tail(cmd[1], done);
   }
   else if(cmd[0] === 'curl'){
-    commands.curl(cmd[1]);
+    commands.curl(cmd[1], done);
   }
 
   //process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
 });
+
+
+var done = (output) => {
+  console.log(output);
+}
--- commands.js
+++ commands.js
@@ -1,62 +1,70 @@
 var fs = require('fs');
 var request = require('request');
 
-function pwd(){
-  console.log(`${process.cwd()}`);
+
+
+function pwd(done){
+  done(`${process.cwd()}`);
+
 };
 
-function date(){
-  console.log(formatDate(new Date()));
+function date(done){
+  done(formatDate(new Date()));
 };
 
-function ls(){
+function ls(done){
   fs.readdir('.', 'utf8', function(err, files){
     if(err) throw err;
 
     files.forEach(function(file){
-      process.stdout.write(file.toString() + '\n');
+      done(file.toString() + '\n');
     });
   });
 };
 
-var echo = (cmd) => {
+var echo = (cmd, done) => {
   var str = cmd.slice(1).join(' ');
-  console.log(str);
+  done(str);
   //return str;
 };
 
-var cat = (filePath) => {
-  fs.readFile(filePath, 'utf8', function (err, data) {
+var cat = (fileName, done) => {
+  var dataToHead = '';
+   fs.readFile(fileName, 'utf8', function (err, data) {
     if(err) throw err;
-   console.log (data);
+   
+   if (typeof done !== undefined) { done (data);
+   }
+   
+   dataToHead = data;
   })
-
-
+  //console.log(dataToHead, " Data to head");
+  return dataToHead;
 }
 
-var head = (filePath) => {
-  fs.readFile(filePath, 'utf8', function(err, data){
-    if(err) throw err;
+var head = (fileName, done) => {
+  var data = cat(fileName);
+  //console.log(data);
     var fileHead = data.split('\n').slice(0, 10).join('\n');
-    console.log(fileHead);
-  })
+    done(fileHead);
+  
 }
 
-var tail = (filePath) => {
+var tail = (filePath, done) => {
   fs.readFile(filePath, 'utf8', function(err, data){
     if(err) throw err;
     var lines = data.split('\n');
     var fileTail = lines.slice(lines.length - 10).join('\n');
-    console.log(fileTail);
+    done(fileTail);
   })
 }
 
-var curl = (urlString) => {
+var curl = (urlString, done) => {
   var urlPath = `http://www.${urlString}`;
   request(urlPath, function(err, response, body){
-    console.log('error;', err);
-    console.log('statuscode:', response && response.statusCode);
-    console.log('body:', body);
+    done(`Error: ${err}`);
+    done(`statuscode: ${response && response.statusCode}`);
+    done(`body: ${body}`);
   });
 };
 
